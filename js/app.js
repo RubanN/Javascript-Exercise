b37e99c4235460f12f57b9ab81d484dae9972f83
Ruban <80041905+RubanN@users.noreply.github.com>
2021-07-07 23:13:46 +0530

    First exercise on clock

---
+++ js/app.js
@@ -0,0 +1,4 @@
+//Write a JavaScript program to display the current day and time in the following format
+var today = new Date();
+var day = today.getDay();
+var dayList = ["sunday",'Monday',"Tuesday","Wednesday","Thursday","Friday","Saturday"];
